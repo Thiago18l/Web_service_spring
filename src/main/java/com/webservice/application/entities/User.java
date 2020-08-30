6d2caf3c92ebf2af777d41b76057a16076ff42d3
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-08-30 17:31:41 -0300

    feat: usign JPA to create a entity and save information on database

--- src/main/java/com/webservice/application/entities/User.java
+++ src/main/java/com/webservice/application/entities/User.java
@@ -1,11 +1,19 @@
 package com.webservice.application.entities;
 
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
     private static  final long serialVersionUID = 1L;
-    private long id;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private long id;
     private String name;
     private String email;
     private String phone;
